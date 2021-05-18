3ffafa318a15049cac33771697751a7431fdf240
Diogo <diogo.crack@gmail.com>
2021-05-18 13:47:38 +0100

    add phpunit feature tests for tasks

--- tests/Feature/TaskTest.php
+++ tests/Feature/TaskTest.php
@@ -4,6 +4,7 @@ namespace Tests\Feature;
 
 use Illuminate\Foundation\Testing\RefreshDatabase;
 use Illuminate\Foundation\Testing\WithoutMiddleware;
+use Illuminate\Testing\TestResponse;
 use Tests\TestCase;
 
 class TaskTest extends TestCase
@@ -11,11 +12,11 @@ class TaskTest extends TestCase
     use WithoutMiddleware;
     
     /**
-     * test get all to-do lists
+     * test get all tasks
      */
     public function testGetAllTasks()
     {
-        $response = $this->get('/todolist/all');
+        $response = $this->get('/tasks/all');
 
         $response->assertStatus(200);
         $this->assertIsNotArray($response);
@@ -23,14 +24,60 @@ class TaskTest extends TestCase
     }
 
     /**
-     * test get all to-do lists
+     * test get all tasks by to-do list
      */
     public function testGetAllTasksByTolist()
     {
-        $response = $this->get('/todolist/all');
+        $response = $this->json(
+            'GET',
+            '/tasks/by-todo-list',
+            ["todoId" => 1]
+        );
 
         $response->assertStatus(200);
         $this->assertIsNotArray($response);
         $this->assertInstanceOf(TestResponse::class, $response); 
+    }
+
+    /**
+     * test create new task
+     */
+    public function testCreateNewTask()
+    {
+        $response = $this->post('/tasks/create', [
+            "todoListId" => 1,
+            "title" => "testeDummyTask",
+            "description" => "testeDummyTask"
+        ]);
+
+        $response->assertStatus(201);
+        $this->assertInstanceOf(TestResponse::class, $response);
+    }
+
+    /**
+     * test update task
+     */
+    public function testUpdateTask()
+    {
+        $response = $this->put('/tasks/update/1', [
+            "todoListId" => 1,
+            "title" => "testeDummyUpdate",
+            "description" => "testeDummyUpdated",
+            "statusId" => 3
+        ]);
+
+        $response->assertStatus(200);
+        $this->assertInstanceOf(TestResponse::class, $response);
+    }
+
+    /**
+     * delete task
+     */
+    public function testDeleteTask()
+    {
+        $response = $this->delete('/tasks/delete/1');
+
+        $response->assertStatus(200);
+        $this->assertInstanceOf(TestResponse::class, $response);
     }
 }
